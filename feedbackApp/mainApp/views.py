83594f7faa996e6f2d0078b45466ded61b51f505
rahula00 <rahul-arora@outlook.com>
2020-06-25 13:19:29 -0700

    testing with vue

--- feedbackApp/mainApp/views.py
+++ feedbackApp/mainApp/views.py
@@ -1,3 +1,19 @@
+import random
+import json
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+
+    items = []
+    for i in range(100):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20,80),
+            "url": "https://example.com",
+        })
+
+    context = {}
+    context["items_json"] = json.dumps(items)
+
+    return render(request, 'list.html', context)
