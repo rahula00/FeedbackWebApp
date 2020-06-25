1c1838f4b46f4fdc42ff501bac285c2a61aa3e27
Rahul Arora <rahul-arora@outlook.com>
2020-06-25 16:59:46 -0700

    url and settings. didnt work last time.

--- feedbackApp/feedbackApp/settings.py
+++ feedbackApp/feedbackApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'feedbackApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
