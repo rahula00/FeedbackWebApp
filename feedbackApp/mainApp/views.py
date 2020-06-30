e467fbba67e6e939818ae8949c241d2756b67880
Rahul Arora <rahul-arora@outlook.com>
2020-06-30 07:44:18 -0700

    Added dbs

--- feedbackApp/mainApp/views.py
+++ feedbackApp/mainApp/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Managers, Feedback
 
 
 def home(request):
