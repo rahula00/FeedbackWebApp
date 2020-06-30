150ac03947d1cd5072d9dde442fc3e0e3a339807
bdreyerr <bendreyer21@gmail.com>
2020-06-29 17:06:34 -0700

    login fix

--- feedbackApp/mainApp/views.py
+++ feedbackApp/mainApp/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-
-
-
-# Create your views here.
 
 def home(request):
     if request.method == 'POST':
@@ -45,6 +42,7 @@ def index(request):
     return render(request, 'list.html', context)
 
 
+@login_required(login_url='homepage')
 def manager(request):
     context = {}
     return render(request, 'manager.html', context)
