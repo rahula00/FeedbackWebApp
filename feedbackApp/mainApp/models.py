521e80cf79cb23d5857a55372e45cf609a4faaf1
Rahul Arora <rahul-arora@outlook.com>
2020-06-30 11:03:50 -0700

    Added submitted_by and issue_type fields to feedback model

--- feedbackApp/mainApp/models.py
+++ feedbackApp/mainApp/models.py
@@ -12,4 +12,11 @@ class Feedback(models.Model):
 	feedback = models.CharField(blank=False, max_length=2000)
 	manager = models.ForeignKey(Managers, blank=True, null=True, on_delete=models.CASCADE)
 	created_at = models.DateTimeField(auto_now_add=True)
-
+	TYPE_CHOICES = [
+        ('S', 'Sales'),
+        ('T', 'Technical'),
+        ('C', 'Coaching'),
+        ('O', 'Other'),
+    ]
+	type_choice = models.CharField(max_length=1, choices=TYPE_CHOICES, default=0)
+	submitted_by = models.CharField(max_length=60, default="Anonymous")
